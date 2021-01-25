c28d6a55472baa6de4679f60156cbd921d23fde2
Greg MAZA <maza.gregory46@gmail.com>
2021-01-25 15:51:28 +0100

    v1.2

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-Bonjour !
+<?php
+date_default_timezone_set('UTC');
+$page = filter_input(INPUT_GET,'action',FILTER_SANITIZE_STRING);
+if ($page) {
+    if ($page == 'accueil') {
+        include 'action/accueil.php';
+    }
+    elseif ($page == 'hobby') {
+        include 'action/hobby.php';
+    }
+    elseif ($page == 'contact') {
+        include 'action/contact.php';
+    }else{
+        echo '404';
+    }
+
+}
+else {
+
+    include 'action/accueil.php';
+}
