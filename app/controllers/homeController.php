94e8532f069eac54b5d660e2a937d7c3f2732754
Greg MAZA <maza.gregory46@gmail.com>
2021-01-26 15:54:57 +0100

    Function and homeController cfg

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,2 +1,6 @@
 <?php
 echo 'HELLO WORLD ! YES';
+
+require  'app/persistences/blogPostData.php';
+$tenLastPosts = lastBlogPosts($pdo);
+var_dump($tenLastPosts);
---
+++ app/persistences/blogPostData.php
@@ -0,0 +1,4 @@
+<?php
+function lastBlogPosts(PDO $connect){
+    return [];
+}
