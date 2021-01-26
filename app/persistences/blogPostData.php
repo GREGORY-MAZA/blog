abb79c4bb0a4def412034afb391888a46cba4122
Greg MAZA <maza.gregory46@gmail.com>
2021-01-26 17:14:36 +0100

    last 26-01

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -2,3 +2,9 @@
 function lastBlogPosts(PDO $connect){
     return [];
 }
+//REQUETE SQL !
+
+// SELECT `title`, `text`, `pseudo`, DATE_FORMAT(`date_begin`,"%d/%m/%Y"), DATE_FORMAT(`date_end`,"%d/%m/%Y")
+//FROM `posts`
+//INNER JOIN `authors` ON `posts`.`authors_id` = `authors`.`id`
+//LIMIT 2
