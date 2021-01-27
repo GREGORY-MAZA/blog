bde865a1c33594cba1f9aa36686b1535e70e6a53
Greg MAZA <gregory.maza@le-campus-numerique.fr>
2021-01-27 16:12:10 +0100

    Last

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -3,6 +3,7 @@ require 'ressources/views/header.tpl.php';
 require 'ressources/views/footer.tpl.php';
 if (!empty($tenLastPosts)) {
     var_dump($tenLastPosts);
+
 }
 else {
     echo 'NADA ! ! ! !';
